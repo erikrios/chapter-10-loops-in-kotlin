a2ea99814754bf480ec33734b8d885e787de93e6
erikrios <erikriosetiawan49@gmail.com>
2019-08-25 19:01:34 +0700

    Looping over iterables

--- src/main/kotlin/com/erikriosetiawan/LoopsOne.kt
+++ src/main/kotlin/com/erikriosetiawan/LoopsOne.kt
@@ -1,2 +1,21 @@
 package com.erikriosetiawan
 
+fun main() {
+    val list = listOf("Hello, ", "World", "!")
+    for (str in list) {
+        print(str)
+    }
+
+    println()
+
+    for (i in 0..9) {
+        println(i)
+    }
+    for ((index, element) in list.withIndex()) {
+        println("$element in index $index")
+    }
+
+    list.forEach {
+        print(it)
+    }
+}
