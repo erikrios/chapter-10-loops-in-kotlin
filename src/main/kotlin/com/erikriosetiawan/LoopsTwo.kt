fe5bf9b0db03f17d22e8b0bde4f6c2931f6a0221
erikrios <erikriosetiawan49@gmail.com>
2019-08-25 19:05:30 +0700

    Repeat an action x times

---
+++ src/main/kotlin/com/erikriosetiawan/LoopsTwo.kt
@@ -0,0 +1,8 @@
+package com.erikriosetiawan
+
+fun main() {
+    repeat(10) { i ->
+        println("This line will be printed 10 times")
+        println("We are on the ${i + 1}. loop iteration")
+    }
+}
